e9d425a22fddbe7d2bdedfa3a206a6cf1eb6e337
Taylor Mohney <taylormohneytech@gmail.com>
2023-01-06 09:58:56 -0800

    starting maxIceCream

---
+++ maxIceCreamBars.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+ var maxIceCream = function(costs, coins) {
+    // sort the array ascending
+    console.log(costs.sort((a, b) => a - b));
+    
+};
+
+
+// Test Case 1
+maxIceCream([1,3,2,4,1], 7) // 4
