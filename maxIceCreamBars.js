74851c4d5b4a6ebf4742e5957e5744b70f95c7d9
Taylor Mohney <taylormohneytech@gmail.com>
2023-01-06 10:19:47 -0800

    solved

--- maxIceCreamBars.js
+++ maxIceCreamBars.js
@@ -4,8 +4,6 @@
  * @return {number}
  */
  var maxIceCream = function(costs, coins) {
-    // sort the array ascending
-    costs.sort((a, b) => a - b);
     // Switch statement to handle edge cases
     switch (true) {
         case costs.length == 0:
@@ -15,10 +13,25 @@
         case costs[0] > coins:
             return 0;
         default:
-            
-    
+            // Create a new array containing only ice cream bars that cost less than coins
+            let affordable = costs
+                .filter(element => element <= coins)
+                .sort((a, b) => a - b);
+                console.log(affordable);
+            let total = 0;
+            let count = 0;
+            for (let i = 0; i < affordable.length; i++) {
+                total += affordable[i];
+                if (total <= coins) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            return count;
+    }
 };
 
 
 // Test Case 1
-maxIceCream([1,3,2,4,1], 7) // 4
+maxIceCream([1,3,2,4,1,10], 7) // 4
