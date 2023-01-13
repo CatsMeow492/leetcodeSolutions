49be36c7d80b230f60e56ac275550d0c4184e77d
Taylor Mohney <taylormohneytech@gmail.com>
2023-01-13 09:56:58 -0800

    binary preorder traversal solved

--- binaryTreePreorderTraversal.js
+++ binaryTreePreorderTraversal.js
@@ -11,16 +11,19 @@
  * @return {number[]}
  */
  var preorderTraversal = function(root) {
-    let number = [];
-    root.forEach(element => {
-        // if element is not null, push to array
-        // if element is null, do nothing
-        if (element !== null) {
-            number.push(element)
-        }
-    });
-    console.log(number)
-};
+    if(!root) return [];
+    let stack = [root]
+    let result = []
+    while (stack.length) {
+        let node = stack.pop()
+        result.push(node.val)
+        if (node.right) stack.push(node.right)
+        if (node.left) stack.push(node.left)
+    }
+    return result
+}
+
 
 // Test Case 1
 preorderTraversal([1,null,2,3]) // [1,2,3]
+
