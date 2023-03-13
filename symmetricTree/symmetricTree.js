053a604dd8e52fcde4820cef15a4cd37242a919b
Taylor Mohney <taylormohneytech@gmail.com>
2023-03-13 13:33:18 -0700

    working on symmetricTree md

--- symmetricTree/symmetricTree.js
+++ symmetricTree/symmetricTree.js
@@ -1,7 +1,29 @@
 /**
- * @param {*} input
- * @returns
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-function symmetricTree(input) {
-  // Your code here
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if (!root) {
+      return true;
+  }
+  return isMirror(root.left, root.right);
+};
+// Helper Function
+let isMirror = function(firstNode, secondNode) {
+      // Depth-First Search
+      if (!firstNode || !secondNode){
+          return firstNode == secondNode;
+      }
+      if (firstNode.val !== secondNode.val) {
+          return false
+      }
+      return isMirror(firstNode.left, secondNode.right) && isMirror(firstNode.right, secondNode.left);
 }
